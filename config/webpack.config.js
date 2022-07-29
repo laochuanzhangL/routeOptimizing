2273cdfbdcc70da52cc63fedfa90410687ce4ab9
小天才 <2782605412@qq.com>
2022-07-29 19:09:27 +0800

    完善配置文件中的less配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -71,6 +71,8 @@ const cssRegex = /\.css$/
 const cssModuleRegex = /\.module\.css$/
 const sassRegex = /\.(scss|sass)$/
 const sassModuleRegex = /\.module\.(scss|sass)$/
+const lessRegex=/\.less$/
+const lessModuleRegex=/\.module\.less$/
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -540,6 +542,30 @@ module.exports = function (webpackEnv) {
                   },
                 },
                 'sass-loader'
+              ),
+            },
+            {
+              test: lessRegex,
+              exclude: sassModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                },
+                'less-loader'
+              ),
+              sideEffects: true,
+            },
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                  modules: true,
+                  getLocalIdent: getCSSModuleLocalIdent,
+                },
+                'less-loader'
               ),
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
--- src/pages/userHome/page/tddisplay/index.js
+++ src/pages/userHome/page/tddisplay/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export const Tddisplay = () => {
-  return <div className="tddisplay">tddisplay</div>
+  return (
+  <div className="tddisplay">
+    tddisplay
+  </div>
+  )
 }
